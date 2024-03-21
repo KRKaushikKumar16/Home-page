db09ea30effe06f17ea6050bf939bc00b64138fd
K R Kaushik Kumar (krkkumar) <krkkumar@cisco.com>
2024-03-21 17:05:38 +0530

    Your commit message

---
+++ constants/index.js
@@ -0,0 +1,87 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'Education',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'Finance',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Business',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Healthcare',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Data Augmentation',
+  },
+];
+
+export const startingFeatures = [
+  'AI Nexus fuses Hugging Face LLMs for intuitive communication.',
+  'LangChain orchestrates AI workflows for seamless task execution.',
+  'GANs drive creative content generation across the platform.',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.png',
+    title: 'Hugging Face',
+    subtitle:
+        'We have incorporated Hugging Face\'s transformative language models into AI Nexus, unveiling a new world of AI possibilities for you to experience with ease.',
+  },
+  {
+    imgUrl: '/headset.png',
+    title: 'Langchain',
+    subtitle:
+        'We have seamlessly incorporated LangChain\'s sophisticated AI orchestration into AI Nexus, offering you a new realm of streamlined intelligence and innovation to explore with confidence.',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Enhanced Interaction with AI',
+    subtitle:
+        ' The integration of Hugging Face\'s language models into AI Nexus provides a seamless and natural user experience, leveraging the power of AI to understand and respond to complex queries with unprecedented accuracy.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Modular AI for Custom Solutions',
+    subtitle:
+        'AI Nexus\'s adoption of LangChain allows for the assembly of bespoke AI solutions, offering users the flexibility to combine various AI models and tools that cater specifically to their unique challenges and objectives.',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Revolutionizing Content Creation',
+    subtitle:
+        'By harnessing the capabilities of Generative Adversarial Networks, AI Nexus stands at the cutting edge of content generation, enabling users to produce highly realistic and diverse outputs that push the boundaries of traditional media and design.',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+    link: 'https://twitter.com/openai?lang=en',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+    link: 'https://www.linkedin.com/company/openai/',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+    link: 'https://www.instagram.com/welcome.ai/?hl=en',
+  },
+];
